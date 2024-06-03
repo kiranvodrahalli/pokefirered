2afbd6177559e0fa7bcd702a070651a8104d1548
Kiran Vodrahalli <kiran.vodrahalli@gmail.com>
2024-06-03 00:12:07 -0700

    Some evolution updates and held item updates. Also prevent roamer from fleeing to avoid roamer glitch.

--- data/battle_ai_scripts.s
+++ data/battle_ai_scripts.s
@@ -3224,14 +3224,7 @@ AI_Unknown_End::
 	end
 
 AI_Roaming::
-	if_status2 AI_USER, STATUS2_WRAPPED, AI_Roaming_End
-	if_status2 AI_USER, STATUS2_ESCAPE_PREVENTION, AI_Roaming_End
-	get_ability AI_TARGET
-	if_equal ABILITY_SHADOW_TAG, AI_Roaming_End
-	get_ability AI_USER
-	if_equal ABILITY_LEVITATE, AI_Roaming_Flee
-	get_ability AI_TARGET
-	if_equal ABILITY_ARENA_TRAP, AI_Roaming_End
+	end
 
 AI_Roaming_Flee::
 	flee
